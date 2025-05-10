0726569933f7f42eee8f8eaee3b6abdadd5856d2
Srichandana1619 <123981274+Srichandana1619@users.noreply.github.com>
2025-05-10 06:14:17 -0500

    Models

---
+++ UserAccount.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BlazorApp.Models
+{
+    public class UserAccount
+    {
+        [Required]
+        public int UserAccountId { get; set; } // Primary key
+
+        [EmailAddress]
+        [Required]
+        public string Email { get; set; } // Changes were made to dbContext to require uniqueness to this attribute. Found in Data/BlazorAppContext.cs
+        [StringLength(100)]
+        public string PasswordHash { get; set; } // Don't store passwords in plaintext!
+        [StringLength(100)]
+        public string FirstName {  get; set; }
+        [StringLength(100)]
+        public string LastName { get; set; }
+        public DateTime CreationDate { get; set; }
+    }
+}
